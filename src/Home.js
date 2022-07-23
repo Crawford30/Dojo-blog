86d4d82cce2c4411ff9794cf85431f28f4874fd8
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-07-23 14:59:54 +0300

    useState to uodate reactive value

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 const Home = () => {
   const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
 
     const handleClick = () => {
         setName('Joel');
+        setAge(80);
      
     }
 
@@ -14,7 +16,7 @@ const Home = () => {
     return (  
         <div className="home">
             <h2>Home page</h2>
-            <p>{name}</p>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click Me</button>
             <button onClick={(e) => {
                 handleClickAgain('joel', e)
