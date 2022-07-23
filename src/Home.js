3e0c74ace3c69ff1ed15d8aab68bc2eacc1900e1
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-07-23 15:17:22 +0300

    Code formatter

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,11 @@
 import { useState } from "react";
 const Home = () => {
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
+        const handleClick = () => {
 
-    const handleClick = () => {
-        setName('Joel');
-        setAge(80);
-     
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
-    }
-
+         }
     return (  
         <div className="home">
             <h2>Home page</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => {
-                handleClickAgain('joel', e)
-            }}>Click Me Again</button>
         </div>
     );
 }
