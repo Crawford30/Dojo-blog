5360a846fe6b91b63fd84135eb49f682afc4f24f
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-07-23 12:06:28 +0300

    Variables

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 function App() {
   const title = "Welcome to the new blog";
+  const likes = 50;
   return (
     <div className="App">
      <div className="content">
       <h1>{title}</h1>
+      <p>Liked {likes} times {Math.random() * 10}</p>
      </div>
     </div>
   );
