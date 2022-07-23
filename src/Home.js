ad5730de7a22057128337b5c986e5beeaaef69ca
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-07-23 13:19:21 +0300

    Invoking a function inside annymouse func for a func which takes an argument

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('hello, Ninjaa');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+
     return (  
         <div className="home">
             <h2>Home page</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => {
+                handleClickAgain('joel')
+            }}>Click Me Again</button>
         </div>
     );
 }
