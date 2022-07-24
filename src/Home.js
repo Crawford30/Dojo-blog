10aae054e4f5be142c243796c9000cebf068936b
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-07-24 14:20:18 +0300

    Fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,34 @@
 import { useState , useEffect} from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-        // { title: 'School Project', body: 'lorem ipsum...', author: 'mario', id: 4 },
-        // { title: 'Leavers party!', body: 'lorem ipsum...', author: 'yoshi', id: 5 },
-        // { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 6 }
-      ]);
+    // const [blogs, setBlogs] = useState([
+    //     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    //     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    //     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
+    //   ]);
+
+
+
+    const [blogs, setBlogs] = useState(null);
 
       const [name, setName] = useState('mario');
     
        
-         const handleDelete = (id) => {
-            const newBlogs = blogs.filter(blog => blog.id  !== id);
-            setBlogs(newBlogs);
-        };
+        //  const handleDelete = (id) => {
+        //     const newBlogs = blogs.filter(blog => blog.id  !== id);
+        //     setBlogs(newBlogs);
+        // };
 
         useEffect(() => {
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+               return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setBlogs(data);
+
+            })
             console.log('use effect');
             console.log(name);
         }, [name]);
@@ -31,9 +41,10 @@ const Home = () => {
     return (  
         <div className="home">
            {/* <BlogList blogs={blogs} title="All Blogs"/>  */}
-           <BlogList blogs={blogs.filter((blog) =>  blog.author === 'mario') } title="Mario's Blogs" handleDelete={handleDelete}/> 
-           <button onClick={() => setName('luigo')}>Change Name</button>
-           <p>{name}</p>
+           {/* {blogs &&  <BlogList blogs={blogs.filter((blog) =>  blog.author === 'mario') } title="Mario's Blogs" handleDelete={handleDelete}/>} */}
+
+           {blogs &&  <BlogList blogs={blogs} title="Mario's Blogs" />}
+          
         </div>
     );
 }
