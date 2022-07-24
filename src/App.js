d9ed278fbf643428c1d5af242bb6bacc917644dd
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-07-24 20:45:03 +0300

    Exact Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -10,8 +12,8 @@ function App() {
       <Navbar/>
      <div className="content">
      <Routes>
-
      <Route exact path="/" element={<Home/>}/>
+     <Route exact path="/create" element={<Create/>}/>
      </Routes>
      </div>
     </div>
