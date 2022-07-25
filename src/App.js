d0c57734e9cac67c7ae7fce7992a00c6846a33a9
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-07-25 08:05:09 +0300

    Not Found Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Create from './Create';
 
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   const title = "Welcome to the new blog";
@@ -16,6 +17,7 @@ function App() {
      <Route exact path="/" element={<Home/>}/>
      <Route exact path="/create" element={<Create/>}/>
      <Route exact path="/blogs/:id" element={<BlogDetails/>}/>
+     <Route exact path="*" element={<NotFound/>}/>
      </Routes>
      </div>
     </div>
